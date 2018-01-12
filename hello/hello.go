d329f44685973621f4c8ed5ae3d074ac235e9ad8
Xavier Berthiaume <xavierberthiaume@Xaviers-MBP.local>
2018-01-12 17:47:50 -0500

    Initial commit

---
+++ hello/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+import "github.com/xavier-berthiaume/stringutil"
+
+func main() {
+
+	fmt.Printf("Hello World!\n")
+	fmt.Printf(stringutil.Reverse("\nHello World!"))
+}
+
---
+++ stringutil/reverse_test.go
@@ -0,0 +1,22 @@
+//Test cases for reverse function
+
+package stringutil
+
+import "testing"
+
+func TestReverse(t *testing.T) {
+
+	cases := []struct {
+		in, want string
+	}{
+		{"Hello World", "dlroW olleH"},
+		{"Hello 世界", "界世 olleH"},
+		{"", ""},
+	}		
+	for _, c := range cases {
+		got := Reverse(c.in)
+		if got != c.want {
+			t.Errorf("Reverse(%q) == %q, want %q", c.in, got, c.want)
+		}	
+	}
+}
