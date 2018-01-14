758b85d76f8b1b92602662ccebf5f0ecf41a3738
Xavier Berthiaume <xavierberthiaume@Xaviers-MBP.local>
2018-01-14 09:32:54 -0500

    - Improved the Get function to properly set http request header, no more 429 error - Added PrintRequest function to handle the printing of data - Fixed the print Struct to properly format Request data

--- reddit/geddit/main.go
+++ reddit/geddit/main.go
@@ -1,17 +1,9 @@
 package main
 
 import (
-	"log"
-	"fmt"
 	"github.com/xavier-berthiaume/reddit"
 )
 
 func main() {
-	items, err := reddit.Get("bartenders")
-	if err != nil {
-		log.Fatal(err)
-	}
-	for _, item := range items {
-		fmt.Println(item.Title)
-	}	
+	reddit.PrintResponse(reddit.Get("bartenders"))
 }
--- reddit/reddit.go
+++ reddit/reddit.go
@@ -6,12 +6,15 @@ import (
         "encoding/json"
         "fmt"
 	"errors"
+	"log"
 )
 
 type Item struct {
         Title string
         URL   string
         Comments int `json:"num_comments"`
+	Score int
+	Subreddit string
 }
 
 
@@ -27,33 +30,47 @@ func (i Item) String() string {
         com := ""
         switch i.Comments {
                 case 0:
-
+			com = " (No Comments Yet!) "
                 case 1:
                         com = " (1 Comment) "
                 default:
                         com = fmt.Sprintf("(%d Comments)", i.Comments)
         }
-        return fmt.Sprintf("%s\n%s\n%s", i.Title, com, i.URL)
+        return fmt.Sprintf("%s\n%s\t%d votes\tr/%s\n%s\n", i.Title, com, i.Score, i.Subreddit, i.URL)
+}
+
+func AcceptInput() {
+
+
+}
+
+func PrintResponse(items []Item, err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, item := range items {
+		fmt.Println(item)
+	}
 }
 
 func Get(reddit string) ([]Item, error) {
+	client := &http.Client{}
 	url := fmt.Sprintf("http://reddit.com/r/%s.json", reddit)
-        resp, err := http.Get(url)
-        if err != nil {
-                return nil, err
-        }
-        defer resp.Body.Close()
-        if resp.StatusCode != http.StatusOK {
-                return nil, errors.New(resp.Status)
-        }
-        r := new(Response)
-        err = json.NewDecoder(resp.Body).Decode(r)
-        if err != nil {
-                return nil, err
-        }
-        items := make([]Item, len(r.Data.Children))
-        for i, child := range r.Data.Children {
-                items[i] = child.Data
-        }
-        return items, nil
+	request, err := http.NewRequest("GET", url, nil)
+	request.Header.Set("User-Agent", "Subreddit Crawler/V1.0")
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+	if response.StatusCode != http.StatusOK {
+		return nil, errors.New(response.Status)
+	}
+	r := new(Response)
+	err = json.NewDecoder(response.Body).Decode(r)
+	items := make([]Item, len(r.Data.Children))
+	for i, child := range r.Data.Children {
+		items[i] = child.Data
+	}
+	return items, nil
 }
